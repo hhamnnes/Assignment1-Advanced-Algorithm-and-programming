05f2df14b224c3bf57f4ae4372a458cc5fc8a691
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2025-02-25 20:29:33 +0100

    I have crated the class FactorialAlgorithm I have done this to ensure that I have one methode which represent the Recursive algorithm and a different one that represent the Iterative one

---
+++ first_assignment/src/main/java/isep/eleve/hamnnes/firstPart/FactorialAlgorithm.java
@@ -0,0 +1,21 @@
+package isep.eleve.hamnnes.firstPart;
+
+public class FactorialAlgorithm {
+
+    public FactorialAlgorithm() {
+    }
+    
+    public static int factorialRecursive(int n) {
+        return n == 0 ? 1 : n * factorialRecursive(n - 1);
+    }
+    
+    public static int factorialIterative(int n) {
+        for(int i = n - 1; i > 0; --i) {
+            n *= i;
+        }
+    
+        return n;
+    }
+
+    
+}

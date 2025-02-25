b2ae892d178ee985fa85074e8d4f26740862d999
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2025-02-25 21:23:09 +0100

    I crated a interface for ExecutionTimeAnalyzing

---
+++ first_assignment/src/main/java/isep/eleve/hamnnes/firstPart/ExecutionTimeAnalyzerForFactorialAlgorithms.java
@@ -0,0 +1,9 @@
+package isep.eleve.hamnnes.firstPart;
+
+import isep.eleve.hamnnes.firstPart.FactorialAlgorithm;
+
+public class ExecutionTimeAnalyzerForFactorialAlgorithms{
+
+
+    
+}
---
+++ first_assignment/src/main/java/isep/eleve/hamnnes/interfaces/ExecutionTimeAnalyzer.java
@@ -0,0 +1,7 @@
+package isep.eleve.hamnnes.interfaces;
+
+public interface ExecutionTimeAnalyzer {
+
+    long[][] measureExecutionTime(int scope, int interval);
+    
+}
